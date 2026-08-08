4eeadf28674e0ed3ddfcab5db3350e3ac2a562d4
alexjet1000 <alexjet1000@gmail.com>
2026-08-08 23:00:17 +0200

    fix: satisfy migration update safety check

--- migrations/0026_aberrant_quentin_quire.sql
+++ migrations/0026_aberrant_quentin_quire.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `games` ADD `update_search_results_available` integer DEFAULT false NOT NULL;--> statement-breakpoint
 ALTER TABLE `games` ADD `packs_search_results_available` integer DEFAULT false NOT NULL;--> statement-breakpoint
-UPDATE games SET update_search_results_available = search_results_available;
+UPDATE games SET update_search_results_available = search_results_available WHERE id IS NOT NULL;
